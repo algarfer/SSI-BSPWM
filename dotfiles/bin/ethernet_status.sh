71bb415bcd8df8a53c434b2c02876451cc98e6cf
algarfer <algarferx@proton.me>
2024-02-11 19:56:40 +0100

    Ahora el modulo de ethernet te indica cuando no tienes conexion

--- dotfiles/bin/ethernet_status.sh
+++ dotfiles/bin/ethernet_status.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "%{F#2495e7} %{F#ffffff}$(/usr/sbin/ifconfig ens33 | grep "inet " | awk '{print $2}')%{u-}"
+ethStatus=$(/usr/sbin/ifconfig ens33 2> /dev/null)
+
+inetP=$(echo "$ethStatus" | grep 'inet ')
+
+if ! [[ -z $inetP ]]; then
+	echo "%{F#2495e7} %{F#ffffff}$(echo "$inetP" | awk '{print $2}')%{u-}"
+else
+	echo "%{F#ff0000} %{F#ffffff}No Network"
+fi
